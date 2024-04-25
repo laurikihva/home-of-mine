12bdf588f2ed7162deb94b05de6d0b0db4dd5763
Lauri <laurikihva@gmail.com>
2024-04-26 00:12:28 +0300

    initial commit

---
+++ src/components/Food.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="text-white">
+    <ul>
+      <li v-for="(food, i) in foodItems" :key="i">
+        {{ food }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script lang="ts" setup>
+const foodItems = ["Pasta", "Pizza", "Potatoes"];
+</script>
